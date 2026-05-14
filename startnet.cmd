f2136acf996fce056410dd1be50220762432e933
FlashiOne <43337858+Flashione@users.noreply.github.com>
2026-05-14 17:26:14 +0200

    Make WindowsPE loader use external start.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -5,39 +5,33 @@ wpeinit
 powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c >nul 2>&1
 
 echo.
-echo Searching for WimTools USB root...
+echo Searching for external startup script...
 echo.
 
 set "USBROOT="
 
 for %%D in (C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: Y: Z:) do (
-    if exist "%%D\WimTools\WIMTOOLS.TAG" (
-        set "USBROOT=%%D"
-        goto :FOUND
+    if /I not "%%D"=="X:" (
+        if exist "%%D\start.cmd" (
+            set "USBROOT=%%D"
+            goto :FOUND
+        )
     )
 )
 
-echo WimTools USB root not found.
-echo Expected marker:
-echo \WimTools\WIMTOOLS.TAG
+echo External startup script not found.
+echo Expected:
+echo \start.cmd
 echo.
 cmd /k
 exit /b 1
 
 :FOUND
-echo Found WimTools at %USBROOT%\WimTools
+echo Found startup script at %USBROOT%\start.cmd
 echo.
 
-if not exist "%USBROOT%\WimTools\startup.cmd" (
-    echo startup.cmd not found:
-    echo %USBROOT%\WimTools\startup.cmd
-    echo.
-    cmd /k
-    exit /b 1
-)
-
-call "%USBROOT%\WimTools\startup.cmd"
+call "%USBROOT%\start.cmd"
 
 echo.
-echo startup.cmd finished or exited.
+echo start.cmd finished or exited.
 cmd /k
